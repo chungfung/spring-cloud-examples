c80131cb6f1b01be0a6d1662b920a506df44ed83
chungfung <fy_java@163.com>
2019-05-14 15:30:49 +0800

    SpringCloud 之producer生产者

--- spring-cloud-eureka-producer/src/main/java/com/chungfung/controller/HelloController.java
+++ spring-cloud-eureka-producer/src/main/java/com/chungfung/controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 	
     @RequestMapping("/hello")
     public String index(@RequestParam String name) {
-        return "hello " + name + "!";
+        return "hello " + name + ",this is from 8001!";
     }
 }
