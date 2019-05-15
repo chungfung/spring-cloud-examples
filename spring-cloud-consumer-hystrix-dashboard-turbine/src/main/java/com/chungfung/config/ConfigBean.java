3f574f15abbb6ec064ae5a214623199f606c15c0
chungfung <fy_java@163.com>
2019-05-15 14:40:35 +0800

    SpringCloud 之hystrix dashboard turbine

---
+++ spring-cloud-consumer-hystrix-dashboard-turbine/src/main/java/com/chungfung/config/ConfigBean.java
@@ -0,0 +1,27 @@
+package com.chungfung.config;
+
+/**
+ * @Description
+ * @Author 丰涌
+ * @Date 2019/5/15 10:39
+ * @Version 1.0
+ */
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigBean {
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
