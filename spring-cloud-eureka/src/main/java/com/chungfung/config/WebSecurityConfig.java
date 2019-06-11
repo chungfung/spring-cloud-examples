49bbc1a1fc4626539c48bdf825396671c2cd886f
chungfung <fy_java@163.com>
2019-06-11 18:00:12 +0800

    SpringCloud 之 eureka server

--- spring-cloud-eureka/src/main/java/com/chungfung/config/WebSecurityConfig.java
+++ spring-cloud-eureka/src/main/java/com/chungfung/config/WebSecurityConfig.java
@@ -15,7 +15,10 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable(); // 关闭csrf
-        http.authorizeRequests().anyRequest().authenticated().and().httpBasic(); // 开启认证
+//        http.csrf().disable(); // 关闭csrf
+//        http.authorizeRequests().anyRequest().authenticated().and().httpBasic(); // 开启认证
+
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
     }
 }
